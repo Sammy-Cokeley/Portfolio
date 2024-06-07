54da7de1027c2327f3c7a291f120da27bddaa479
sammy-cokeley <sammy.cokeley@syntax.com>
2024-06-07 11:14:38 -0500

    removed node_modules from repo and preliminary page changes

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <h1>I'm Home!</h1>;
+  var text = ['C','o','k','e','l','e','y'];
+
+
+
+
+  return(
+    <div class='name-container'>
+      <div class='line'></div>
+      <div class="name">
+        {text.map((item) => (
+          <span id={item}>{item}</span>
+        ))}
+      </div>
+      <div class='line reverse'></div>
+    </div>
+  );
 };
 
 export default Home;
