39b2fe6d173c42b9f76081d0afaaee0a35323923
sammy-cokeley <sammy.cokeley@syntax.com>
2024-06-07 13:13:25 -0500

    linting and removed dumb lines

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,11 @@ const Home = () => {
 
   return (
     <div className="name-container">
-      <div className="line"></div>
       <div className="name">
         {text.map((item) => (
           <span key={item}>{item}</span>
         ))}
       </div>
-      <div className="line reverse"></div>
       <p className="short-bio">
         My name is Sammy Cokeley, I am a software developer based in Daphne, Alabama.<br />
         I work primarily in End-To-End Automation using DevOps practices.<br />
